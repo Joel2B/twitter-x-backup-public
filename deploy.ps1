a97b13a4facdc7faf5086eae901fb79672931d76
Joel2B <jbuenouas@gmail.com>
2026-04-17 03:06:34 -0700

    Fix Docker build context and stop deploy on errors

--- deploy.ps1
+++ deploy.ps1
@@ -1,2 +1,15 @@
-docker build -t 192.168.2.141:5000/twitter-x-backup:latest .
-docker push 192.168.2.141:5000/twitter-x-backup:latest
+$ErrorActionPreference = "Stop"
+
+$image = "192.168.2.141:5000/twitter-x-backup:latest"
+
+docker build -t $image .
+
+if ($LASTEXITCODE -ne 0) {
+    throw "docker build failed with exit code $LASTEXITCODE"
+}
+
+docker push $image
+
+if ($LASTEXITCODE -ne 0) {
+    throw "docker push failed with exit code $LASTEXITCODE"
+}
