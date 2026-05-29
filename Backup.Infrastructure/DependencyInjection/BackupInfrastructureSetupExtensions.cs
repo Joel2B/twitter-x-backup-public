7448e86ff45a7a00ccd14cb867b66ae73f37816b
Joel2B <jbuenouas@gmail.com>
2026-05-28 23:32:33 -0700

    refactor(infrastructure): move setup/runtime registrations into DI modules

--- Backup.Infrastructure/DependencyInjection/BackupInfrastructureSetupExtensions.cs
+++ Backup.Infrastructure/DependencyInjection/BackupInfrastructureSetupExtensions.cs
@@ -1,9 +1,35 @@
-using Backup.App.Extensions;
+using Backup.App.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace Backup.Infrastructure.DependencyInjection;
 
 public static class BackupInfrastructureSetupExtensions
 {
-    public static Task RunBackupInfrastructureSetup(this IServiceProvider provider) =>
-        provider.RunSetup();
+    public static async Task RunBackupInfrastructureSetup(this IServiceProvider provider)
+    {
+        IEnumerable<ISetup> setups = provider.GetServices<ISetup>();
+
+        foreach (ISetup setup in setups)
+        {
+            ILogger logger = provider
+                .GetRequiredService<ILoggerFactory>()
+                .CreateLogger(setup.GetType());
+
+            if (string.IsNullOrWhiteSpace(setup.Id))
+            {
+                logger.LogInformation("Setup: {SetupClass}", setup.GetType().Name);
+            }
+            else
+            {
+                logger.LogInformation(
+                    "[{SetupId}] Setup: {SetupClass}",
+                    setup.Id,
+                    setup.GetType().Name
+                );
+            }
+
+            await setup.Setup();
+        }
+    }
 }
--- Backup.Infrastructure/DependencyInjection/FeatureInfrastructureServiceCollectionExtensions.cs
+++ Backup.Infrastructure/DependencyInjection/FeatureInfrastructureServiceCollectionExtensions.cs
@@ -1,4 +1,17 @@
+using Backup.App.Data.Media;
+using Backup.App.Data.Proxy;
+using Backup.App.Interfaces.Services.Media;
+using Backup.App.Interfaces.Services.Posts;
+using Backup.App.Interfaces.Services.UtilsService;
 using Backup.App.Extensions;
+using Backup.App.Interfaces;
+using Backup.App.Interfaces.Data.Proxy;
+using Backup.App.Interfaces.Proxy;
+using Backup.App.Services.Proxy;
+using Backup.App.Services.Bulk;
+using Backup.App.Services.Media;
+using Backup.App.Services.Posts;
+using Backup.App.Services.UtilsService;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Backup.Infrastructure.DependencyInjection;
@@ -34,20 +47,36 @@ public static class FeatureInfrastructureServiceCollectionExtensions
 
     public static IServiceCollection AddRuntimeServicesInfrastructure(this IServiceCollection services)
     {
-        services.AddUtils();
-        services.AddServices();
+        services.AddSingleton<IZipWriterFactory, ZipWriterFactory>();
+        services.AddSingleton<IBandwidthLimiter, BandwidthLimiter>();
+
+        services.AddScoped<IPostService, PostService>();
+        services.AddScoped<IMediaService, MediaService>();
+        services.AddScoped<IBulkService, BulkService>();
+
         return services;
     }
 
     public static IServiceCollection AddSetupInfrastructure(this IServiceCollection services)
     {
-        services.AddSetup();
+        services.AddScoped<LocalProxyData>();
+        services.AddScoped<IProxyData>(sp => sp.GetRequiredService<LocalProxyData>());
+        services.AddScoped<ISetup>(sp => sp.GetRequiredService<LocalProxyData>());
+
+        services.AddScoped<ProxyProvider>();
+        services.AddScoped<IProxyProvider>(sp => sp.GetRequiredService<ProxyProvider>());
+        services.AddScoped<ISetup>(sp => sp.GetRequiredService<ProxyProvider>());
+
+        services.AddScoped<LocalMediaLogger>();
+        services.AddScoped<IMediaLogger>(sp => sp.GetRequiredService<LocalMediaLogger>());
+        services.AddScoped<ISetup>(sp => sp.GetRequiredService<LocalMediaLogger>());
+
         return services;
     }
 
     public static IServiceCollection AddAppRuntimeInfrastructure(this IServiceCollection services)
     {
-        services.AddApp();
+        services.AddScoped<global::Backup.App.App>();
         return services;
     }
 }
