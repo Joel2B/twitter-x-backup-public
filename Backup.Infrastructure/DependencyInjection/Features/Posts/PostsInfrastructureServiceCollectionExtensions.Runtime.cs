0c59287926ad11bf2863d319a6bb92f19db98cfe
Joel2B <jbuenouas@gmail.com>
2026-05-30 11:20:23 -0700

    refactor: finish namespace-folder alignment for remaining infrastructure files

--- Backup.Infrastructure/DependencyInjection/Features/Posts/PostsInfrastructureServiceCollectionExtensions.Runtime.cs
+++ Backup.Infrastructure/DependencyInjection/Features/Posts/PostsInfrastructureServiceCollectionExtensions.Runtime.cs
@@ -1,5 +1,5 @@
 using Backup.Infrastructure.Posts.Abstractions.Services;
-using Backup.Infrastructure.Posts.Models;
+using Backup.Infrastructure.Posts.Abstractions.Data;
 using Backup.Infrastructure.Posts.Adapters;
 using Microsoft.Extensions.DependencyInjection;
 
--- Backup.Infrastructure/Posts/Abstractions/Data/IPostDownload.cs
+++ Backup.Infrastructure/Posts/Abstractions/Data/IPostDownload.cs
@@ -1,7 +1,6 @@
-using Backup.Infrastructure.Posts.Abstractions.Data;
 using Backup.Infrastructure.Models.Config.Api;
 
-namespace Backup.Infrastructure.Posts.Models;
+namespace Backup.Infrastructure.Posts.Abstractions.Data;
 
 public interface IPostDownload
 {
